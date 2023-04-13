d2415ef8bfec28062ef2ad3817112bd686e1a6eb
Valentina <valentinakachi@gmail.com>
2023-04-13 19:36:05 -0300

    Desafio 1, de nuevo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola coder</Text>
+      <Text>Hola, coder!</Text>
       <StatusBar style="auto" />
     </View>
   );
